76dcce3d32864e4ecaafcffb084cee5df60156e6
Denis Gabbasov <gabbas.denn@mail.ru>
2023-02-27 18:46:06 +0300

    feat:переписать MarvelService и RandomChar с взаимодейтвием со своим хуком

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,32 +1,35 @@
+import { useHttp } from "../hooks/http.hook";
 
+const useMarvelService = () => {
 
-class MarvelService {
-  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiKey = 'apikey=8eba837de29e2ca3122d673d4dd7e5a2';
-  _baseOffsetChar = 600;
+  const {loading, request, error} = useHttp();
 
-  getResource = async (url) => {
-    let res = await fetch(url);
+  const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  const _apiKey = 'apikey=8eba837de29e2ca3122d673d4dd7e5a2';
+  const _baseOffsetChar = 600;
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    }
+  // getResource = async (url) => {
+  //   let res = await fetch(url);
 
-    return await res.json();
+  //   if (!res.ok) {
+  //     throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+  //   }
+
+  //   return await res.json();
+  // }
+
+  const getAllCharacters = async (offset = _baseOffsetChar) => {
+    const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+
+    return res.data.results.map(_transformCharacter);
   }
 
-  getAllCharacters = async (offset = this._baseOffsetChar) => {
-    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-
-    return res.data.results.map(this._transformCharacter);
+  const getCharacter = async (id) => {
+    const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+    return _transformCharacter(res.data.results[0]);
   }
 
-  getCharacter = async (id) => {
-    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-    return this._transformCharacter(res.data.results[0]);
-  }
-
-  _transformCharacter = (char) => {
+  const _transformCharacter = (char) => {
     return {
       name: char.name,
       description: char.description ?  `${char.description.slice(0, 210)}...` : 'No description',
@@ -37,6 +40,8 @@ class MarvelService {
       comics: char.comics.items.slice(0, 10)
     }
   }
+
+  return {loading, error, getAllCharacters, getCharacter}
 }
 
-export default MarvelService;
+export default useMarvelService;
